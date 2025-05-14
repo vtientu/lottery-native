0e87387bd81a64defe5bd34ffb52a5bc5386636f
TuDev <tientu115@gmail.com>
2025-05-14 17:15:55 +0700

    Update fetch api news

--- services/news.service.js
+++ services/news.service.js
@@ -1,6 +1,10 @@
 import mainApiRequest from "./mainApiRequest";
 
 export const getNews = async () => {
-  const response = await mainApiRequest.get("/news");
-  return response.data;
+  try {
+    const response = await mainApiRequest.get("/news");
+    return response.data;
+  } catch (error) {
+    throw new Error(error.message || "Failed to get news");
+  }
 };
